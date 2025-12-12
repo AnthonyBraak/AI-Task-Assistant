d05ac77e5c21897359bd287497f1d6938f4719ee
AnthonyBraak <anthony.braak0@gmail.com>
2025-12-12 16:34:49 +0100

    Fix global SCSS import

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // optional, convenient
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "/src/styles/_variables.scss";`,
+        additionalData: `@use "@/styles/variables" as *;`, // use alias @
       },
     },
   },
